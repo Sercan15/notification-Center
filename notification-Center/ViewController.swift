aae88981850ed91463a90e7e741d5345aceaa63f
Sercan Yeşilyurt <sercanyesilyurts@gmail.com>
2024-11-14 09:07:03 +0300

    first commit

--- notification-Center/ViewController.swift
+++ notification-Center/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var codeTextField: UITextField!
+    @IBOutlet weak var decryptButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
 
+    @IBAction func decryptButtonTapped(_ sender: UIButton) {
+        
+        guard let enteredCode = codeTextField.text, enteredCode == "1234" else {
+            let alert = UIAlertController(title: "Incorrect Code", message: "Please enter the correct code to proceed.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+            return
+        }
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 15) {
+            let message = "Decoded Secret Message"
+            NotificationCenter.default.post(name: NSNotification.Name("DecryptionComplete"), object: nil, userInfo: ["message": message])
+        }
+        
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
 }
 
